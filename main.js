03ee282a38b525c495e1de648e2d9a8606d9ed88
AKG Office <abguven@yahoo.com>
2020-12-22 17:56:51 +0100

    testing 3th canvas Test number 1

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ canvas = document.getElementById('canvas2');
 ctx = canvas.getContext('2d');
 ctx.fillStyle = 'red';
 ctx.fillRect(10, 10, 100, 100);
+
+canvas = document.getElementById('canvas3');
+ctx = canvas.getContext('2d');
+ctx.fillStyle = 'orange';
+ctx.fillRect(20, 20, 100, 100);
+
