2f35fa97e43f9cc11485704c242196904189aa09
AKG Office <abguven@yahoo.com>
2020-12-22 18:50:21 +0100

    adding 3th canvas

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ ctx = canvas.getContext('2d');
 ctx.fillStyle = 'red';
 ctx.fillRect(10, 10, 100, 100);
 
+
 canvas = document.getElementById('canvas3');
 ctx = canvas.getContext('2d');
-ctx.fillStyle = 'yellow';
-ctx.fillRect(20, 20, 100, 100);
-
+ctx.fillStyle = 'orange';
+ctx.fillRect(40, 40, 100, 100);
