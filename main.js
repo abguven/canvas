5c8b8952e2ce7e32bc315a6bacbc0be837d3e964
AKG Office <abguven@yahoo.com>
2020-12-22 17:46:36 +0100

    Adding a second canvas, also controlled by javascript

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ var canvas = document.getElementById('canvas1');
 var ctx = canvas.getContext('2d');
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 100, 100);
+
+
+canvas = document.getElementById('canvas2');
+ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(10, 10, 100, 100);
